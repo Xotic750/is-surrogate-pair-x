a18f53847012758238833e39f766bb9be2c5f0ff
Graham Fairweather <xotic750@gmail.com>
2019-07-24 15:23:25 +0200

    :bookmark: v3.0.17
    Add jsdelivr

--- dist/is-surrogate-pair-x.js
+++ dist/is-surrogate-pair-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-23T23:43:02.664Z",
+  "date": "2019-07-24T13:23:20.006Z",
   "describe": "",
   "description": "Tests if 2 characters together are a surrogate pair.",
   "file": "is-surrogate-pair-x.js",
   "hash": "71ada446baccbbdda154",
   "license": "MIT",
-  "version": "3.0.16"
+  "version": "3.0.17"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
