c19cb5cbb1f4df4193a9a68f0b900de5bf4f6f6d
Graham Fairweather <xotic750@gmail.com>
2019-07-27 15:27:20 +0200

    :bookmark: v3.0.22
    Auto build

--- dist/is-surrogate-pair-x.js
+++ dist/is-surrogate-pair-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-27T00:23:51.037Z",
+  "date": "2019-07-27T13:27:15.116Z",
   "describe": "",
   "description": "Tests if 2 characters together are a surrogate pair.",
   "file": "is-surrogate-pair-x.js",
   "hash": "71ada446baccbbdda154",
   "license": "MIT",
-  "version": "3.0.21"
+  "version": "3.0.22"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
